4a10b1b865cedc4ff3a0870cf4aa720a3cbcf821
Kay <kard@hotmail.co.uk>
2022-07-04 16:53:59 +0100

    removed comments in notesmodel and test

--- notesModel.test.js
+++ notesModel.test.js
@@ -40,16 +40,3 @@ describe('NotesModel',() => {
     expect(model.getNotes()).toEqual(['Buy milk']);
   })
 })
-
-// const model = new NotesModel();
-
-// model.getNotes(); // should return []
-
-// model.addNote('Buy milk');
-// model.addNote('Go to the gym');
-
-// model.getNotes(); // should now return ['Buy milk', 'Go to the gym']
-
-// model.reset();
-
-// model.getNotes(); // should now return []
